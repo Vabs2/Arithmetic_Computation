00d3c8c8ae416ebe0daef4913f192f8f40aad6fe
Vabs2 <vaibhavkadbhane2@gmail.com>
2020-10-23 08:49:36 +0530

    Calculating Equation1

--- Arithmetic_Calculation.sh
+++ Arithmetic_Calculation.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
-read -p "Enter the a input" input1
-read -p "Enter the b input" input2
-read -p "Enter the c input" input3
+function input()
+{
+read -p "Enter the a input " input1
+read -p "Enter the b input " input2
+read -p "Enter the c input " input3
+
+}
+
+function firstEquation()
+{
+        input
+        firstEquationResult=$(( $input1 + $input2 * $input3))
+        echo $firstEquationResult
+}
+firstEquation
