f845c0ce7c432e5d7adacd16c14e24f8980dbd1b
Vabs2 <vaibhavkadbhane2@gmail.com>
2020-10-23 09:12:24 +0530

    Sorting in Ascending order

--- Arithmetic_Calculation.sh
+++ Arithmetic_Calculation.sh
@@ -34,6 +34,7 @@ function readValuesToArray()
                 arrayElements[index]=${storeResult[$index]}
         done
         descendingSort
+        ascendingSort
 }
 
 function descendingSort()
@@ -54,5 +55,22 @@ function descendingSort()
         echo Array In Descending Order..${arrayElements[@]}
 }
 
+function ascendingSort()
+{
+        temp=0
+        for ((i=0; i<${#arrayElements[@]}; i++ ))
+        do
+                for ((j=i+1; j<${#arrayElements[@]}; j++ ))
+                do
+                        if [[ ${arrayElements[i]%.*} -gt ${arrayElements[j]%.*} ]]
+                        then
+                                temp=${arrayElements[i]}
+                                arrayElements[i]=${arrayElements[j]}
+                                arrayElements[j]=$temp
+                        fi
+                done
+        done
+        echo Array In Ascending Order..${arrayElements[@]}
+}
 getInputs
 readValuesToArray
